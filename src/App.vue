171aa9efa611f326e9bba4d26c413779f77dcfbe
rangga <ranggaaldosas@gmail.com>
2022-12-04 12:01:57 +0700

    boilerplate

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="text-6xl font-bold underline">
+    Hello world!
+  </h1>
+  <RouterView />
+</template>
+
