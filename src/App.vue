d5ea40dda6f61505a0be487852f0c7ed036a7e2e
rangga <ranggaaldosas@gmail.com>
2022-12-04 12:15:12 +0700

    update template

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,6 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <h1 class="text-6xl font-bold underline">
-    Hello world!
-  </h1>
   <RouterView />
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
-  <main>
-    <h1 class="text-3xl font-bold underline">
-    Rangga
-  </h1>
-  </main>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-6xl font-bold">
+    Hello world!</h1>
+    <p class="text-2xl mt-4">VueJS-Tailwind boilerplate</p>
+    <img src="https://www.datocms-assets.com/48401/1628644787-tailwind-vue.png" alt="">
+  </div>
+  
 </template>
